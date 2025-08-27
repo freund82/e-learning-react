ac9c835867e3987f835ea15d15a023a6da41fe2b
Maxim Utkin <maxim2001_2001@mail.ru>
2025-08-27 17:22:02 +0300

    Готов блок с карточками Featured Courses

--- src/pages/Home/FeaturedCourses/FeaturedCourses.jsx
+++ src/pages/Home/FeaturedCourses/FeaturedCourses.jsx
@@ -1,8 +1,10 @@
 import "./featuredCourses.css"
 import {useState} from "react"
+import {Link} from "react-router-dom"
 import featuredCourses from "../../../data/featuredCourses"
 import ButtonSecondary from "../../../components/shared/ButtonSecondary/ButtonSecondary"
 import WeeksIcon from "../../../assets/icons/weeks.svg"
+import StudentsIcon from "../../../assets/icons/students.svg"
  
 
 function FeaturedCourses() {
@@ -29,11 +31,22 @@ function FeaturedCourses() {
                                 <div className="featuredCourses__item--card" key={item.id}>
                                     <img className="featuredCourses__item--img" src={item.img} alt={item.title} />
                                     <div className="featuredCourses__item--inner">
-                                        <p>by {item.author}</p>
+                                        <p>by <span className="featuredCourses__item--author">{item.author}</span></p>
                                         <h3 className="featuredCourses__item--title">{item.title}</h3>
-                                        <span className="featuredCourses__item--text"><img src={WeeksIcon} alt="weeks" />{item.weeks} Weeks</span>
-                                        <span className="featuredCourses__item--text">{item.students} Students</span>
-                                        <p className="featuredCourses__item--text">{item.price} Students</p>
+                                        <div className="featuredCourses__item--info">
+                                            <img src={WeeksIcon} alt="weeks" />
+                                            <span className="featuredCourses__item--text">{item.length}Weeks</span>
+                                             <img src={StudentsIcon} alt="students" />
+                                            <span className="featuredCourses__item--text">{item.students} Students</span>
+                                        </div>
+                                        <div className="featuredCourses__item--price">
+                                            <div>
+                                                 <span className="featuredCourses__item--text" style={item.free || item.newPrice? {textDecoration:"line-through", fontSize:"var(--font-size18)", color:"var(--gray)", fontWeight:"400"}:""}>${item.price.toFixed(1)}</span>
+                                            {item.free==true?<span className="featuredCourses__item--text" style={{marginLeft:"0.6rem", color:"var(--green)", fontSize:"var(--font-size18)", fontWeight:"600"}}>Free</span>:""} {/*Условный рендеринг*/}
+                                            {item.newPrice?<span className="featuredCourses__item--text" style={{color:"var(--red)", fontSize:"var(--font-size18)", fontWeight:"600", marginLeft:"0.6rem"}}>${item.newPrice.toFixed(1)}</span>:""}
+                                            </div>
+                                            <Link to="/viewmore" className="featuredCourses__item--link" href="#" style={{fontSize:"var(--font-size18)", fontWeight:"600", color:"var(--black)"}}>View More</Link>
+                                        </div>
                                     </div>
                                 </div>
                             ))
