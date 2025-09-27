1f9a3d70b5b924b901e1afc1a594594b1eb7ec1d
Maxim Utkin <maxim2001_2001@mail.ru>
2025-09-27 16:35:10 +0300

    Добавил base в vite-config.js и также прописал basename в файл main.jsx в BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/e-learning-react/">
     <App />
     </BrowserRouter>
   </StrictMode>,
