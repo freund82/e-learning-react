249bb873eed9efe581d8b8358f39a8009d6e15b7
Maxim Utkin <maxim2001_2001@mail.ru>
2026-01-14 22:53:39 +0300

    all-courses/Добавил с помощью grid два столбца и в первом столбце три строки

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,10 +1,22 @@
-
+import "./courses.css"
 
 function Courses() {
     return (
-        <div>
-            Courses
-        </div>
+        <section className="all-courses">
+            <div className="container">
+                    <div className="all-courses__inner">
+                        <div className="left-section"> 
+                            <h1>All Courses</h1>
+                        </div>
+                        <div className="courses-row">
+                            
+                        </div>
+                        <div className="right-section">
+                            <h1>Filters</h1>
+                        </div>
+                    </div>
+            </div>
+        </section>
     )
 }
 
