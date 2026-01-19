a478ed3d2f93497acc0c3b44d724254444f112d7
Maxim Utkin <maxim2001_2001@mail.ru>
2026-01-19 22:59:37 +0300

    all-courses/Сделал простую пагинацию но пока не доконца стилизовал и нет клювиков кнопок < и >

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -2,6 +2,7 @@ import "./courses.css"
 import Card from "../../components/shared/Card/Card.jsx"
 import courses from "../../data/courses.js"
 import Search from "../../components/shared/Search/Search.jsx"
+import Pagination from "../../components/shared/Pagination/Pagination.jsx"
 import { useState } from "react"
 
 
@@ -9,25 +10,36 @@ import { useState } from "react"
 function Courses() {
 
     const [listStyle, setListStyle] = useState(false)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [coursesPerPage] = useState(6)
 
     const activeIconValue = (value) => {
        setListStyle(value);
     };
 
+    const indexOfLastCourse = currentPage * coursesPerPage;
+    const indexOfFirstCourse = indexOfLastCourse - coursesPerPage;
+    const currentCourses = courses.slice(indexOfFirstCourse, indexOfLastCourse);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
     return (
         <section className="all-courses">
             <div className="container">
                     <div className="all-courses__inner">
-                        <div className="left-section"> 
+                        <div className="left-section">
                             <h1>All Courses</h1>
                             <Search activeIconValue={activeIconValue}/>
                         </div>
                         <div className={`courses-row ${listStyle ? "list" : ""}`}>
-                            <Card courses={courses} isList={listStyle}/>
+                            <Card courses={currentCourses} isList={listStyle}/>
                         </div>
                         <div className="courses-pagination">
-                            <span>1</span>
-                            <span>2</span>
+                            <Pagination
+                            coursesPerPage={coursesPerPage}
+                            totalCourses={courses.length}
+                            paginate={paginate}
+                        />
                         </div>
                         <div className="right-section">
                             <h1>Course Category</h1>
