b7c3bb8808bf327512cc6e318c90757ab3a06260
Maxim Utkin <maxim2001_2001@mail.ru>
2026-01-30 23:03:08 +0300

    all-courses-filters/Добавил звезды рейтинга и чекбоксы. Но пока без фильрации по звездности и кол-во оценок

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,5 +1,7 @@
 import courses from "../../data/courses";
 import "./filters.css";
+import StarYellowSvg from "../../assets/icons/starYellow.svg"
+import StarGreySvg from "../../assets/icons/starGrey.svg"
 
 
 function Filters({coursesCategoryFilter,
@@ -101,6 +103,127 @@ function Filters({coursesCategoryFilter,
                 </div>
             );
         })}
+        {/*Рейтинг*/}
+        <h2>Rating</h2>
+        <div className="rating">
+            {/*Пять звезд*/}
+            <div>
+                <span>
+                    <input type="checkbox" id="stars5" name="stars"  value="5" onChange={(e) => console.log(e.target.checked, e.target.value)} />
+                    <label htmlFor="stars">
+                            <span>
+                                {[1, 2, 3, 4, 5].map((star) => (
+                                    <img
+                                        key={star}
+                                        src={StarYellowSvg}
+                                        alt={`Star ${star}`}
+                                    />
+                                ))}
+                            </span>
+                    </label>
+                </span>
+            </div>
+            {/*Четыре звезды*/}
+            <div>
+                <span>
+                    <input type="checkbox" id="stars4" name="stars" value="4" onChange={(e) => console.log(e.target.checked, e.target.value)} />
+                    <label htmlFor="stars">
+                            <span>
+                                {[1, 2, 3, 4, 5].map((star, index) => (
+                                    index < 4 ? (
+                                        <img
+                                            key={star}
+                                            src={StarYellowSvg}
+                                            alt={`Star ${star}`}
+                                        />
+                                        ) : (
+                                            <img
+                                                key={star}
+                                                src={StarGreySvg}
+                                                alt={`Star ${star}`}
+                                            />
+                                        )
+                                    ))}
+                            </span>
+                    </label>
+                </span>
+            </div>
+            {/*Три звезды*/}
+            <div>
+                <span>
+                    <input type="checkbox" id="stars3" name="stars" value="3" onChange={(e) => console.log(e.target.checked, e.target.value)} />
+                    <label htmlFor="stars">
+                            <span>
+                                {[1, 2, 3, 4, 5].map((star, index) => (
+                                    index < 3 ? (
+                                        <img
+                                            key={star}
+                                            src={StarYellowSvg}
+                                            alt={`Star ${star}`}
+                                        />
+                                        ) : (
+                                            <img
+                                                key={star}
+                                                src={StarGreySvg}
+                                                alt={`Star ${star}`}
+                                            />
+                                        )
+                                    ))}
+                            </span>
+                    </label>
+                </span>
+            </div>
+            {/*Две звезды*/}
+            <div>
+                <span>
+                    <input type="checkbox" id="stars2" name="stars" value="2" onChange={(e) => console.log(e.target.checked, e.target.value)}/>
+                    <label htmlFor="stars">
+                            <span>
+                                {[1, 2, 3, 4, 5].map((star, index) => (
+                                    index < 2 ? (
+                                        <img
+                                            key={star}
+                                            src={StarYellowSvg}
+                                            alt={`Star ${star}`}
+                                        />
+                                        ) : (
+                                            <img
+                                                key={star}
+                                                src={StarGreySvg}
+                                                alt={`Star ${star}`}
+                                            />
+                                        )
+                                    ))}
+                            </span>
+                    </label>
+                </span>
+            </div>
+            {/*Одна звезда*/}
+            <div>
+                <span>
+                    <input type="checkbox" id="stars1" name="stars" value="1" onChange={(e) => console.log(e.target.checked, e.target.value)}/>
+                    <label htmlFor="stars">
+                            <span>
+                                {[1, 2, 3, 4, 5].map((star, index) => (
+                                    index < 1 ? (
+                                        <img
+                                            key={star}
+                                            src={StarYellowSvg}
+                                            alt={`Star ${star}`}
+                                        />
+                                        ) : (
+                                            <img
+                                                key={star}
+                                                src={StarGreySvg}
+                                                alt={`Star ${star}`}
+                                            />
+                                        )
+                                    ))}
+                            </span>
+                    </label>
+                </span>
+            </div>
+        </div>
     </div>
     );
 }
