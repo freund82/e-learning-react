403e4dbf43a1e77901848435944885d6d3c89266
Maxim Utkin <maxim2001_2001@mail.ru>
2026-02-07 19:16:14 +0300

    layout-nav/Добавил поле под header но выводится только на страницах Courses и Blog через использование useLocation в Layout компоненте

---
+++ src/components/HeaderBottom/HeaderBottom.jsx
@@ -0,0 +1,9 @@
+import "./headerBottom.css";
+
+function HeaderBottom() {
+    return (
+        <div className="header__bottom"></div>
+    );
+}
+
+export default HeaderBottom;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,28 @@
 import "./layout.css";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import { Outlet } from "react-router-dom";
+import HeaderBottom from "../HeaderBottom/HeaderBottom";
+import { Outlet, useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
+   const location = useLocation();
+
+   // Массив путей, где должен показываться HeaderBottom
+  const pagesWithHeaderBottom = [
+    '/courses',
+    '/blog',
+    // можно добавить другие пути
+  ];
+
+    // Проверяем текущий путь
+  const showHeaderBottom = pagesWithHeaderBottom.some(path => 
+    location.pathname.startsWith(path)
+  );
+
    return( 
    <>
         <Header />
+        {showHeaderBottom && <HeaderBottom />}
         <main><Outlet /></main>
         <Footer />
     </>
