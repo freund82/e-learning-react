2d2dbc6f55cc7e36a287744f7396fbd60728d11e
Maxim Utkin <maxim2001_2001@mail.ru>
2026-05-01 21:24:55 +0300

    pageBranch/Активный вопрос открывает ответ по клику

--- src/components/FAQs/FAQs.jsx
+++ src/components/FAQs/FAQs.jsx
@@ -1,14 +1,27 @@
 import "./faqs.css";
 import faqs from "../../data/faqs";
+import { useState } from "react";
 
 function FAQs() {
+
+    const [activeId, setActiveId] = useState(null);
+
+    const handleToggle = (id) => {
+        setActiveId(prevId => prevId === id ? null : id);
+    };
+
     return (
         <div className="container">
             <div className="faqs">
                 <h1 className="faqs__title">FAQs</h1>
                     <ul className="faqs__list">
                         {faqs.map((faq) => (
-                            <li key={faq.id} className="faqs__item">{faq.question}
+                            <li
+                                key={faq.id}
+                                className={`faqs__item ${activeId === faq.id ? "active" : ""}`}
+                                onClick={() => handleToggle(faq.id)}
+                            >
+                                <span>{faq.question}</span>
                                 <svg className="faqsArrowIcon" width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M1.175 0L0 1.175L3.81667 5L0 8.825L1.175 10L6.175 5L1.175 0Z"/>
                                 </svg>
--- src/data/faqs.js
+++ src/data/faqs.js
@@ -7,43 +7,43 @@ const faqs = [
   },
   {
     id: 2,
-    question: 'What is the best way to learn?',
+    question: 'What Does Royalty Free Mean?',
     answer:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
   },
   {
     id: 3,
-    question: 'What is the best way to learn?',
+    question: 'What Does Royalty Free Mean?',
     answer:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
   },
   {
     id: 4,
-    question: 'What is the best way to learn?',
+    question: 'What Does Royalty Free Mean?',
     answer:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
   },
   {
     id: 5,
-    question: 'What is the best way to learn?',
+    question: 'What Does Royalty Free Mean?',
     answer:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
   },
   {
     id: 6,
-    question: 'What is the best way to learn?',
+    question: 'What Does Royalty Free Mean?',
     answer:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
   },
   {
     id: 7,
-    question: 'What is the best way to learn?',
+    question: 'What Does Royalty Free Mean?',
     answer:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
   },
   {
     id: 8,
-    question: 'What is the best way to learn?',
+    question: 'What Does Royalty Free Mean?',
     answer:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
   },
