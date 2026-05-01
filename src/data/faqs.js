75863c762228bf63fd7098835af3af1b3ef7fbfa
Maxim Utkin <maxim2001_2001@mail.ru>
2026-05-01 17:52:28 +0300

    pageBranch/Создал Js файл данных по faqs

---
+++ src/data/faqs.js
@@ -0,0 +1,52 @@
+const faqs = [
+  {
+    id: 1,
+    question: 'What Does Royalty Free Mean?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
+  },
+  {
+    id: 2,
+    question: 'What is the best way to learn?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
+  },
+  {
+    id: 3,
+    question: 'What is the best way to learn?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
+  },
+  {
+    id: 4,
+    question: 'What is the best way to learn?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
+  },
+  {
+    id: 5,
+    question: 'What is the best way to learn?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
+  },
+  {
+    id: 6,
+    question: 'What is the best way to learn?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
+  },
+  {
+    id: 7,
+    question: 'What is the best way to learn?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
+  },
+  {
+    id: 8,
+    question: 'What is the best way to learn?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis faucibus odio arcu duis dui, adipiscing facilisis. Urna, donec turpis egestas volutpat. Quisque nec non amet quis. Varius tellus justo odio parturient mauris curabitur lorem in.',
+  },
+];
+
+export default faqs;
